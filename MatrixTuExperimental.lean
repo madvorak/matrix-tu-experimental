1a862f441dddfdb5cbb4c384f86d708d853388f3
madvorak <martin.dvorak@matfyz.cz>
2024-12-18 10:04:08 +0100

    specifically for Fin 2

--- MatrixTuExperimental.lean
+++ MatrixTuExperimental.lean
@@ -26,7 +26,30 @@ theorem Matrix.isTotallyUnimodular_of_isBlockDiagonal {m n : Type*}
         (fun (J : { j : n // γ j = a }) => J.val)
       ).IsTotallyUnimodular):
     A.IsTotallyUnimodular := by
+  intro k f g hf hg
   sorry
+
+theorem Matrix.isTotallyUnimodular_of_isBlockDiagonal2 {m n : Type*}
+    {A : Matrix m n R} {ι : m → Fin 2} {γ : n → Fin 2}
+    (hA : A.IsBlockDiagonal ι γ)
+    (hAa : ∀ a : Fin 2, (A.submatrix
+        (fun (I : { i : m // ι i = a }) => I.val)
+        (fun (J : { j : n // γ j = a }) => J.val)
+      ).IsTotallyUnimodular):
+    A.IsTotallyUnimodular := by
+  intro k f g hf hg
+  have hA₀ := hAa 0
+  have hA₁ := hAa 1
+  rw [Matrix.isTotallyUnimodular_iff] at hA₀ hA₁
+  clear hAa
+  if hxy : Fintype.card { i₁ : Fin k // ι (f i₁) = 0 }
+         = Fintype.card { j₁ : Fin k // γ (g j₁) = 0 }
+         ∧ Fintype.card { i₂ : Fin k // ι (f i₂) = 1 }
+         = Fintype.card { j₂ : Fin k // γ (g j₂) = 1 }
+  then -- the square case
+    sorry
+  else -- non-square cases
+    sorry
 
 theorem Matrix.fromBlocks_isTotallyUnimodular {m₁ m₂ n₁ n₂ : Type*}
     [Fintype m₁] [DecidableEq m₁] [Fintype n₁] [DecidableEq n₁]
@@ -34,7 +57,7 @@ theorem Matrix.fromBlocks_isTotallyUnimodular {m₁ m₂ n₁ n₂ : Type*}
     {A₁ : Matrix m₁ n₁ R} (hA₁ : A₁.IsTotallyUnimodular)
     {A₂ : Matrix m₂ n₂ R} (hA₂ : A₂.IsTotallyUnimodular) :
     (fromBlocks A₁ 0 0 A₂).IsTotallyUnimodular := by
-  apply Matrix.isTotallyUnimodular_of_isBlockDiagonal (Matrix.fromBlocks_isBlockDiagonal A₁ A₂)
+  apply Matrix.isTotallyUnimodular_of_isBlockDiagonal2 (Matrix.fromBlocks_isBlockDiagonal A₁ A₂)
   intro k
   fin_cases k
   · have hA :
